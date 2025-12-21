124cfefb1327269ac7caebd406c699abf81f6329
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-22 03:08:34 +0530

    add context

---
+++ app/_context/DataContext.jsx
@@ -0,0 +1,12 @@
+import { useContext,createContext } from "react"
+
+const DataContext = () => {
+const DataProvider = createContext();
+
+
+  return (
+    <div>DataContext</div>
+  )
+}
+
+export default DataContext
